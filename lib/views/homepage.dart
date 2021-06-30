43a5fa39d6d0203fb477e00c23232febf20e80d8
ViniciusPi <augusto.vinicius@aluno.ifsp.edu.br>
2021-06-30 12:22:45 -0300

    adding colors

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -8,12 +8,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool c = true;
+  int c = 1;
   funcColor() {
     switch (c) {
-      case true:
+      case 1:
         return Colors.amber;
+      case 2:
+        return Colors.red;
       default:
+        c = 0;
         return Colors.blue;
     }
   }
@@ -21,18 +24,61 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff1e1e1e),
       appBar: AppBar(
         backgroundColor: funcColor(),
         title: Text('this title sucks'),
       ),
       body: Center(
-        child: FlatButton(
-            onPressed: () {
-              setState(() {
-                c = !c;
-              });
-            },
-            child: Text('Oi')),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: SizedBox(
+                height: 30,
+                child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {
+                      setState(() {
+                        c = 1;
+                      });
+                    },
+                    child:
+                        Text('Amber', style: TextStyle(color: Colors.white))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: SizedBox(
+                height: 30,
+                child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      setState(() {
+                        c = 2;
+                      });
+                    },
+                    child: Text('Red', style: TextStyle(color: Colors.white))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: SizedBox(
+                height: 30,
+                child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      setState(() {
+                        c = 0;
+                      });
+                    },
+                    child: Text('Blue', style: TextStyle(color: Colors.white))),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
